45af7e01936fe230751f46a6d1eb1b1028f2a5f1
labarruffe <lucas_barruffe@hotmail.com>
2019-07-18 02:42:18 -0300

    Add project service and http service to provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectOwnerService } from './services/project-owner.service';
 import { VolunteerService } from './services/volunteer.service';
+import { ProjectService } from './services/project.service';
 import { ProjectOwnerComponent } from './components/project-owner/project-owner.component';
 import { ProjectComponent } from './components/project/project.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -19,10 +22,12 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [
     ProjectOwnerService,
     VolunteerService,
+    ProjectService,
   ],
   bootstrap: [AppComponent]
 })
