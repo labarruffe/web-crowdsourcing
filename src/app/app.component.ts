af497bb335e5ceb2f34ad93b8318f7030272f880
labarruffe <lucas_barruffe@hotmail.com>
2019-07-18 01:37:22 -0300

    Develop breadcrumb flow with first step structure done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ProjectOwner } from './models/project-owner';
+import { ProjectOwnerService } from './services/project-owner.service';
+import { Volunteer } from './models/volunteer';
+import { VolunteerService } from './services/volunteer.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web-crowdsourcing';
+  stepControl = 1;
+  titleOfSecondStep = '';
+
+  constructor(
+    private projectOwnerService: ProjectOwnerService,
+    private volunteerService: VolunteerService,
+  ){}
+
+  chooseRole(fullName: string, isProjectOwner: boolean) {
+    this.stepControl = 2;
+    if (isProjectOwner) {
+      this.titleOfSecondStep = 'Cadastrar projeto';
+      const projectOwner = new ProjectOwner('', fullName);
+      this.projectOwnerService.post(projectOwner).subscribe(
+        (response) => {
+          alert(response);
+        },
+        (error) => {
+          alert(error);
+        }
+      );
+    } else {
+      this.titleOfSecondStep = 'Voluntariar-se em um projeto';    
+      const volunteer = new Volunteer('', fullName);
+      this.volunteerService.post(volunteer).subscribe(
+        (response) => {
+          alert(response);
+        },
+        (error) => {
+          alert(error);
+        }
+      );  
+    }
+  }
 }
