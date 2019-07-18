6d3298740b866515928e3387b9359f66c8b6ac45
labarruffe <lucas_barruffe@hotmail.com>
2019-07-17 23:55:26 -0300

    Add project owner service to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectOwnerService } from './services/project-owner.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProjectOwnerService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
